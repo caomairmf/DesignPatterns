bd842a700f4db709213fdb5cf92b1ce86c82f676
Robert M. Flynn <caomairmf@gmail.com>
2017-01-11 14:51:56 -0500

    First Commit of AdapterPattern

---
+++ AdapterPattern/src/AudioPlayer.java
@@ -0,0 +1,28 @@
+
+public class AudioPlayer implements MediaPlayer {
+
+	MediaAdapter mediaAdapter;
+
+	@Override
+	public void play(String audioType, String filename) 
+	{
+		// inbuilt support to play mp3 music files
+		if (audioType.equalsIgnoreCase("mp3")) 
+		{
+			System.out.println("Playing mp3 file. Name: " + filename);
+		}
+		// mediaAdapter is providing support to play other file formats
+		else if (audioType.equalsIgnoreCase("vlc") 
+				|| audioType.equalsIgnoreCase("mp4")) 
+		{
+			mediaAdapter = new MediaAdapter(audioType);
+			mediaAdapter.play(audioType, filename);
+		} 
+		else {
+			System.out.println("Invalid media. " 
+					+ audioType + " format not supported");
+		}
+
+	}
+
+}
